e889aa76fccae5df21da7602ef8a3d6e86d0301b
ebinmj <ebinmathewsjohn@gmail.com>
2026-06-03 12:07:05 +0530

    Add homepage route

--- my project/backend/server.js
+++ my project/backend/server.js
@@ -7,6 +7,9 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Tribal Backend is Running ");
+});
 
 /* ---------- MODELS ---------- */
 
