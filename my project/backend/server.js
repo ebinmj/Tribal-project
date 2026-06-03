ba1a1b0e5edae440d18e19f542b41ee63360389f
ebinmj <ebinmathewsjohn@gmail.com>
2026-06-04 01:22:26 +0530

    Fix booking page

--- my project/backend/server.js
+++ my project/backend/server.js
@@ -40,9 +40,7 @@ mongoose
 
 /* ---------- EMAIL ---------- */
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS
